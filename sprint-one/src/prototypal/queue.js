ad49a988fa61f3e4017d0e3fbcceb0a250834799
underaj <chingyang91@gmail.com>
2016-06-08 20:07:01 -0700

    Prototypal pattern Queue completed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.head = 0;
+  instance.tail = 0;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  dequeue: function() {
+    if (this.head < this.tail) {
+      var val = this.storage[this.head];
+      this.head++;
+      return val;
+    }
+  },
+  enqueue: function(val) {
+    this.tail++;
+    this.storage[this.tail - 1] = val;
+  },
+  size: function() {
+    return this.tail - this.head;
+  }
+};
 
 
