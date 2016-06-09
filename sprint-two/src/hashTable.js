598db83e444095df2ca82d2511fd8d4cf61b6ecd
underaj <chingyang91@gmail.com>
2016-06-09 16:40:51 -0700

    Passed 4 tests on Hashtable prompt

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,15 +5,25 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, k);
+  var bucket;
+  var tuple = [k, v];
+  if (this._storage.get(index) === undefined) {
+    // insert bucket
+
+    bucket = this._storage.set(index, );
+  } else {
+
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
@@ -24,4 +32,10 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+// Before inserting, check if the 
 
+// this._storage.each(function(bucket, i, storage) {
+//   if (i === index) {
+
+//   }
+// });
