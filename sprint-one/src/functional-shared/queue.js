48ad5b71f1d64842e45ef6295b2bbdc35f46b93e
underaj <chingyang91@gmail.com>
2016-06-08 19:33:33 -0700

    functional-shared is now complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    head: 0,
+    tail: 0,
+    storage: {}
+  };
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.tail] = value;
+    this.tail++;
+  },
+  dequeue : function(){
+    if(this.head < this.tail){
+      var value = this.storage[this.head];
+      this.head++;
+      return value;
+    }
+  },
+  size : function(){
+    return this.tail - this.head;
+  }
+};
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackObj = {
+    key: 0
+  };
+  _.extend(stackObj, stackMethods);
+  return stackObj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this[this.key] = value;
+    this.key += 1;
+  },
+  pop: function() {
+    if (this.key < 0) {
+      return undefined;  
+    } else {
+      var popped = this[this.key - 1];
+      if (this.key === 0) {
+      } else {
+        this.key--;
+      }
+      return popped;
+    }
+  },
+  size: function() {
+    return this.key;
+  }
+};
 
 
