be714c4964e9a31ece42744768ccae3587c989e7
underaj <chingyang91@gmail.com>
2016-06-09 10:18:29 -0700

    Completed all tests for Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +12,26 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  console.log(Tree(value));
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  var searchTree = function(node) {
+    console.log(node.value);
+    if (node.value === target) {
+      result = true;
+    }
+    if (node.children.length > 0) {
+      for (var i = 0; i < node.children.length; i++) {
+        searchTree(node.children[i]);
+      }
+    }
+  };
+
+  searchTree(this);
+  return result;
 };
 
 
@@ -21,3 +39,10 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//newtree.value === target return true
+// if there are children loop through
+//else loop through children
+// call function at children[i]
+
+// 
